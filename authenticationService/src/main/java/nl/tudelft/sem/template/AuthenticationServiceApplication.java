ab914e8d7b30da3c5c42ffc8fab5465a15dfa3b9
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-11-24 21:47:40 +0100

    Added rest template

--- authenticationService/src/main/java/nl/tudelft/sem/template/AuthenticationServiceApplication.java
+++ authenticationService/src/main/java/nl/tudelft/sem/template/AuthenticationServiceApplication.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -10,22 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AuthenticationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() { return new RestTemplate(); }
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
-	@RestController
-	static class TestController {
-		@GetMapping("")
-		public String serviceInstancesByApplicationName() {
-			return "test from authenticationService";
-		}
-	}
+    @RestController
+    static class TestController {
+        @GetMapping("")
+        public String serviceInstancesByApplicationName() {
+            return "test from authenticationService";
+        }
+    }
 }
