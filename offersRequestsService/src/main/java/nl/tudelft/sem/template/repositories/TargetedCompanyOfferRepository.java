c0f8f3f0c0eee4a58ed16076baac279228cbcbac
Dan Sochirca <D.Sochirca@student.tudelft.nl>
2021-11-26 00:50:22 +0100

    Added Targeted + nonTargeted companyOffer repositories

--- offersRequestsService/src/main/java/nl/tudelft/sem/template/repositories/TargetedCompanyOfferRepository.java
+++ offersRequestsService/src/main/java/nl/tudelft/sem/template/repositories/TargetedCompanyOfferRepository.java
@@ -1,2 +1,51 @@
-package nl.tudelft.sem.template.repositories;public class TargetedCompanyOfferRepository {
+package nl.tudelft.sem.template.repositories;
+
+import java.util.List;
+import javax.transaction.Transactional;
+import lombok.NonNull;
+import nl.tudelft.sem.template.entities.TargetedCompanyOffer;
+import nl.tudelft.sem.template.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TargetedCompanyOfferRepository extends JpaRepository<TargetedCompanyOffer, Long> {
+    TargetedCompanyOffer findByIdEquals(Long id);
+
+    List<TargetedCompanyOffer> findAllByStudentIdEquals(String studentId);
+
+    List<TargetedCompanyOffer> findAllByStudentIdEqualsAndStatusEquals(
+            String studentId, Status status);
+
+    void deleteById(@NonNull Long id);
+
+    //------------
+    // UPDATES:
+    //------------
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE TargetedCompanyOffer o SET o.hoursPerWeek = ?2 WHERE o.id = ?1")
+    void updateHoursPerWeek(Long studentOfferId, double hoursPerWeek);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE TargetedCompanyOffer o SET o.totalHours = ?2 WHERE o.id = ?1")
+    void updateTotalHours(Long studentOfferId, double totalHours);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE TargetedCompanyOffer o SET o.expertise = ?2 WHERE o.id = ?1")
+    void updateExpertise(Long studentOfferId, List<String> expertise);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE TargetedCompanyOffer o SET o.status = ?2 WHERE o.id = ?1")
+    void updateStatus(Long studentOfferId, Status status);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE TargetedCompanyOffer o SET o.requirements = ?2 WHERE o.id = ?1")
+    void updateRequirements(Long studentOfferId, List<String> requirements);
+
 }
