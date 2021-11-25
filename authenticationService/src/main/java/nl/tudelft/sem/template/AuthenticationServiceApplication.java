dda5d607a51ebe6cf997d834a03752271ae7ad5e
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-11-25 14:51:30 +0100

    Added password encoder bean

--- authenticationService/src/main/java/nl/tudelft/sem/template/AuthenticationServiceApplication.java
+++ authenticationService/src/main/java/nl/tudelft/sem/template/AuthenticationServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +24,11 @@ public class AuthenticationServiceApplication {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @RestController
