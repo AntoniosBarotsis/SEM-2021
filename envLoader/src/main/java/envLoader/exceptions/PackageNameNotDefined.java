3ec6a8018e46edfd55b7289e7b06ce2abc66606b
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-11-29 15:16:57 +0100

    Removed @Serial

--- envLoader/src/main/java/envLoader/exceptions/PackageNameNotDefined.java
+++ envLoader/src/main/java/envLoader/exceptions/PackageNameNotDefined.java
@@ -1,9 +1,6 @@
 package envLoader.exceptions;
 
-import java.io.Serial;
-
 public class PackageNameNotDefined extends RuntimeException {
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public PackageNameNotDefined() {
