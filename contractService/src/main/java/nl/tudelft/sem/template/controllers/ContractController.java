2da31ad9c1ff75442d5791f658be5d3fbb39cca2
Dan Sochirca <D.Sochirca@student.tudelft.nl>
2021-12-03 13:42:36 +0100

    Minor changes

--- contractService/src/main/java/nl/tudelft/sem/template/controllers/ContractController.java
+++ contractService/src/main/java/nl/tudelft/sem/template/controllers/ContractController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/contracts")
 public class ContractController {
 
     @Autowired
