8a89d4babf4b7ec1658beab50f8c4f3448c49477
Dan Sochirca <D.Sochirca@student.tudelft.nl>
2021-12-07 16:22:18 +0100

    Contract Request class.

---
+++ contractService/src/main/java/nl/tudelft/sem/template/DTOs/requests/ContractRequest.java
@@ -0,0 +1,32 @@
+package nl.tudelft.sem.template.DTOs.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nl.tudelft.sem.template.entities.Contract;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ContractRequest {
+    @NotBlank(message = "Company is mandatory.")
+    String companyId;
+    @NotBlank(message = "Student is mandatory.")
+    String studentId;
+    @NotNull(message = "Please specify the hours per week.")
+    double hoursPerWeek;
+    @NotNull(message = "Please specify the total hours.")
+    double totalHours;
+    @NotNull(message = "Please specify the price per hour.")
+    double pricePerHour;
+
+    public Contract toContract(){
+        return new Contract(null, companyId, studentId, null,
+                null, hoursPerWeek, totalHours, pricePerHour, null);
+    }
+}
