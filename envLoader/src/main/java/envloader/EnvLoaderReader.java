5006957bc9913d6f1351013be63eb44861d3ef47
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-12-09 22:24:04 +0100

    Added missing javadoc

--- envLoader/src/main/java/envloader/EnvLoaderReader.java
+++ envLoader/src/main/java/envloader/EnvLoaderReader.java
@@ -9,6 +9,14 @@ import java.util.List;
 import java.util.Locale;
 
 public class EnvLoaderReader {
+    /**
+     * Reads the contents of the given file and returns it as a list of lines.
+     *
+     * @param directory The directory the file is located at relative to the root.
+     * @param filename The filename.
+     * @return A list of the lines.
+     * @throws FileNotFoundException If the file is not found
+     */
     public List<String> read(String directory, String filename) throws FileNotFoundException {
         var location = (directory + "/" + filename).toUpperCase(Locale.ROOT);
         var path = Path.of(new File(location).getAbsolutePath());
