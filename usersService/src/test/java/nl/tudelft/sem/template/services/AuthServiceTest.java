74d178055d8be28238d4b869fa5a31fe271c3541
Dan Sochirca <D.Sochirca@student.tudelft.nl>
2022-01-19 18:25:27 +0100

    100% mutation coverage on authService class

--- usersService/src/test/java/nl/tudelft/sem/template/services/AuthServiceTest.java
+++ usersService/src/test/java/nl/tudelft/sem/template/services/AuthServiceTest.java
@@ -5,21 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import nl.tudelft.sem.template.entities.JwtConfig;
 import nl.tudelft.sem.template.entities.StudentFactory;
 import nl.tudelft.sem.template.entities.User;
-import nl.tudelft.sem.template.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
 
 
 @SpringBootTest
@@ -28,6 +31,9 @@ public class AuthServiceTest {
 
     @Autowired
     private transient AuthService authService;
+
+    @MockBean
+    private JwtConfig jwtConfig;
 
     private transient User user;
 
@@ -55,16 +61,51 @@ public class AuthServiceTest {
 
     @Test
     void testGenerateJwtToken() {
+        when(jwtConfig.getJwtSecret()).thenReturn("secret");
+        when(jwtConfig.getLifetime()).thenReturn((long) (60 * 60 * 1000));
+
         String token = authService.generateJwtToken(user);
         assertNotNull(token);
         DecodedJWT decodedToken = JWT.decode(token);
+
         assertEquals(user.getUsername(), decodedToken.getClaim("userName").asString());
         assertEquals(user.getRole().toString(), decodedToken.getClaim("userRole").asString());
     }
 
     @Test
-    void testDefaultAdminPassword() {
-        String authPassword = authService.getAdminPassword();
-        assertNotNull(authPassword);
+    void testGenerateJwtTokenTestDate(){
+        when(jwtConfig.getJwtSecret()).thenReturn("secret");
+        when(jwtConfig.getLifetime()).thenReturn((long) (60 * 60 * 1000));
+
+        //Mock the instant time clock:
+        String instantExpected = "2025-12-22T10:15:30Z";
+        Clock clock = Clock.fixed(Instant.parse(instantExpected), ZoneId.of("UTC"));
+        Instant instant = Instant.now(clock);
+
+        try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+            mockedStatic.when(Instant::now).thenReturn(instant);
+            Instant now = Instant.now();
+            assertEquals(now.toString(), instantExpected);
+        }
+        //----------------------------
+
+        String token = authService.generateJwtToken(user);
+        assertNotNull(token);
+        DecodedJWT decodedToken = JWT.decode(token);
+
+        Date date = decodedToken.getExpiresAt();
+        Date dateExpected = new Date(Instant.now().toEpochMilli() + (60 * 60 * 1000));
+        assertEquals(dateExpected.toString(), date.toString());
     }
+
+    @Test
+    void testDefaultAdminPassword() {
+        when(authService.getAdminPassword()).thenReturn("password");
+
+        String password = authService.getAdminPassword();
+
+        assertEquals("password", password);
+        verify(jwtConfig, atLeast(1)).getAdminPassword();
+    }
+
 }
